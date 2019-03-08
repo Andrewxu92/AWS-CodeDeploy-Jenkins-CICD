ae6def7cf84d2318845513d472cacf256e494f1c
Andrewxu92 <andrewxu1992@gmail.com>
2019-03-08 09:21:47 +0800

    update picture

--- index.php
+++ index.php
@@ -37,7 +37,7 @@
     $zone = @file_get_contents("http://169.254.169.254/latest/meta-data/placement/availability-zone");
     $private_id = @file_get_contents("http://instance-data/latest/meta-data/local-ipv4");
   ?>
-    <img src="https://s3.cn-north-1.amazonaws.com.cn/bjsdemo/CodeDeploy-Application-Code/awschina.png" width="300"/>
+    <img src="https://s3.cn-north-1.amazonaws.com.cn/rgc-solution-server/WX_HC_Program/LOGs/2019-03-08_091905.png" width="300"/>
     </br>
     <h2>EC2 Instance ID: <?php echo $instance_id; ?></h2>
     <h2>EC2 Zone: <?php echo $zone; ?></h2>
